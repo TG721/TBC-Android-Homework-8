651aff5d96ce354c77774f97b11f97f8e08104b0
Tengiz <Tengiz152@gmail.com>
2022-07-08 01:07:27 +0400

    created visual design (2 layouts) for the app. The design is programmatically very detailed and component divided

---
+++ app/src/main/java/com/example/tbc_homework_8/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.tbc_homework_8
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import com.example.tbc_homework_8.databinding.ActivityMainBinding
+
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+       var mainTextView = binding.mainTitle
+       var text = getString(R.string.play_the_best_ngames_everyday_nwith_witcher_now)
+       var spannableString = SpannableString(text)
+       val  fcsGreen: ForegroundColorSpan =  ForegroundColorSpan(Color.GREEN)
+        spannableString.setSpan(fcsGreen,36, 43, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        mainTextView.setText(spannableString)
+
+
+    }
+}
